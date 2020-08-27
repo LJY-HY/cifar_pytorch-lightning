c6d16ec8a0c94bb7a68a22798c9fea117a637f73
LeeJaeyoon <dlwodbs1421@gmail.com>
2020-08-27 14:16:41 +0900

    add Densenet model

--- train.py
+++ train.py
@@ -11,7 +11,7 @@ from utils.args import *
 
 if __name__ == '__main__':
     datasets = ['CIFAR10','CIFAR100']
-    NNModels = ['VGG','Resnet','WideResnet']
+    NNModels = ['VGG','Resnet','WideResnet','Densenet']
     for dataset in datasets:
         if dataset == 'CIFAR10':
             dm = CIFAR10DataModule()
